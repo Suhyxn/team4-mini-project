419c1c17f44df6dc08d50aa755fc1c56383c2ab7
Suhyxn <momentomori21489@gmail.com>
2022-08-25 19:59:19 +0900

    chore: 전역스타일 설정

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,21 +1,9 @@
-const font = {
-  family: {
-    sansSerif: "'Noto Sans KR', sans-serif",
-    serif: "'Bodoni Moda', serif",
-  },
-  size: {
-    sm: '0.85rem',
-    base: '1rem',
-    lg: '1.2rem',
-    xl: '1.5rem',
-    title: '2.5rem',
-    banner: '10vw',
-  },
+const palette = {
+  baseColor: '##13BD7E',
 }
 
-const color = {}
-
-export const theme = {
-  font,
-  color,
+const theme = {
+  palette,
 }
+
+export default theme
