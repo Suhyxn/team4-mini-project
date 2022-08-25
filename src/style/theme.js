43a50c6249b8dccead00cfbe26d3b31ed5c71da4
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-25 20:57:29 +0900

    Update theme.js

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,7 +1,16 @@
+const size = {
+  sm: '0.85rem',
+  base: '1rem',
+  lg: '1.2rem',
+  xl: '1.5rem',
+  title: '2.5rem',
+  banner: '10vw',
+}
 const palette = {
   baseColor: '##13BD7E',
 }
 
 export const theme = {
+  size,
   palette,
 }
