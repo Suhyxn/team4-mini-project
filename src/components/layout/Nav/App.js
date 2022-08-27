666103ae1ad2b09c7388234505db0b973b7c4b45
park-jeyeon <pjeyeonz@gmail.com>
2022-08-27 12:28:26 +0900

    nav btn

---
+++ src/components/layout/Nav/App.js
@@ -0,0 +1,17 @@
+  import React from 'App';
+  import './App.css';
+  import TempNav from './component/Nav';
+
+
+  function App() {
+      return (
+        <>
+          <main id="main">
+            main
+          </main>
+          <Nav/>
+        </>
+      );
+  }
+
+  export default App;
--- src/components/layout/Nav/index.jsx
+++ src/components/layout/Nav/index.jsx
@@ -1,35 +1,3 @@
-import { NavLink } from 'react-router-dom'
-import * as S from './style'
-
-function Nav() {
-  return (
-    <S.Container>
-      <NavLink
-        style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
-        to="/"
-      >
-        Home
-      </NavLink>
-      <NavLink
-        style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
-        to="/recommened"
-      >
-        Recommended
-      </NavLink>
-      <NavLink
-        style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
-        to="/favorites"
-      >
-        Favorite
-      </NavLink>
-      <NavLink
-        style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
-        to="/mypage"
-      >
-        마이페이지
-      </NavLink>
-    </S.Container>
-  )
-}
-
-export default Nav
+import React from 'react'
+import App from './App'
+import style from './style'
