b995ab28db8282e8c89b9e873ecbf91a807a8673
YANGDAYOUNG <jc1525@hanmail.net>
2022-08-29 00:15:53 +0900

    #18 fix: VITE_BASE_URL

--- src/api/favoriteApiSlice.js
+++ src/api/favoriteApiSlice.js
@@ -1,13 +1,11 @@
 //즐겨찾기(관심상품) 조회, 수정, 삭제
 //즐겨찾기(관심상품) 조회, 수정, 삭제
-const { VITE_SERVICE_KEY } = import.meta.env
+const { VITE_BASE_URL } = import.meta.env
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const favoritesApi = createApi({
   reducerPath: 'favoritesApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: VITE_SERVICE_KEY,
-  }),
+  baseQuery: fetchBaseQuery({ baseUrl: VITE_BASE_URL }),
   endpoints: (builder) => ({
     getFavorites: builder.query({
       query: () => 'favorites',
--- src/api/productApiSlice.js
+++ src/api/productApiSlice.js
@@ -1,10 +1,10 @@
 //상품 조회, 신청, 취소
 //추천상품 조회
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
+const { VITE_BASE_URL } = import.meta.env
 export const productsSlice = createApi({
   reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: VITE_BASE_URL }),
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => '/products',
