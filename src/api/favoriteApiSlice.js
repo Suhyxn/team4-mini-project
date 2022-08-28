f04faf9a52dd933d3066c0205d56b58fee514a1b
YANGDAYOUNG <jc1525@hanmail.net>
2022-08-28 23:02:10 +0900

    #18 feat: favoritesApi get

--- src/api/favoriteApiSlice.js
+++ src/api/favoriteApiSlice.js
@@ -1,1 +1,18 @@
 //즐겨찾기(관심상품) 조회, 수정, 삭제
+//즐겨찾기(관심상품) 조회, 수정, 삭제
+const { VITE_SERVICE_KEY } = import.meta.env
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const favoritesApi = createApi({
+  reducerPath: 'favoritesApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: VITE_SERVICE_KEY,
+  }),
+  endpoints: (builder) => ({
+    getFavorites: builder.query({
+      query: () => 'favorites',
+    }),
+  }),
+})
+
+export const { useGetFavoritesQuery } = favoritesApi
