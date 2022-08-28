af124b257506dd30bc611281ecc8b6c2196e6f9e
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-28 14:56:56 +0900

    #42 fix: 라디오 그룹설정 수정

--- src/components/common/Radio/index.jsx
+++ src/components/common/Radio/index.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import * as S from './style'
 
 function Radio({ items }) {
+  console.log(items)
   return (
     <S.Container>
       {items.map((item, index) => (
         <S.List key={index}>
-          <S.Input type="radio" value={item} name={`group-${item}`} id={item} />
+          <S.Input
+            type="radio"
+            value={item}
+            name={`group-${items}`}
+            id={item}
+          />
           <S.Label htmlFor={item} key={index}>
             <S.Text>{item}</S.Text>
           </S.Label>
