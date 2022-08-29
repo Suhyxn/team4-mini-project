2660a3bb07b1ee1c21931d750d5da879a33db837
YANGDAYOUNG <jc1525@hanmail.net>
2022-08-29 11:23:14 +0900

    #47 feat: 장바구니 데이터 get하기
    컴포넌트 별로 채크가 있고 없고가 다름... 장바구니에서는 다 체크 박스 &
    좋아요 필요함
    스타일링... 이야기하기..!

--- src/routes/Cart/index.jsx
+++ src/routes/Cart/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useGetCartQuery } from '../../store/slices/cartApiSlice'
 import Card from '../../components/common/Card'
+import Loan from '../../components/common/Loan'
+import Loader from '../../components/layout/Loader'
+import Button from '../../components/common/Button'
+import * as S from './style'
 
 function Cart() {
   const { data: carts, isLoading, isError } = useGetCartQuery()
   if (isLoading) {
-    return <div>로딩중...</div>
+    return (
+      <div>
+        <Loader />
+      </div>
+    )
   }
 
   if (isError || !carts) {
@@ -14,9 +22,27 @@ function Cart() {
 
   return (
     <div>
-      {carts.card.map((item) => (
-        <Card item={item} key={item.product_id} />
-      ))}
+      <S.CartsTitleContainer>
+        <S.CartsTitle>
+          <S.TitleSpan>000님</S.TitleSpan>의 장바구니
+        </S.CartsTitle>
+      </S.CartsTitleContainer>
+      <div>
+        <div>
+          {carts.card.map((item) => (
+            <Card item={item} key={item.product_id} />
+          ))}
+        </div>
+
+        <div>
+          {carts.loan.map((item) => (
+            <Loan item={item} key={item.loan_id} />
+          ))}
+        </div>
+      </div>
+      <Button width="100%" height="5.2rem" disabled>
+        장바구니 신청
+      </Button>
     </div>
   )
 }
--- src/routes/Cart/style.js
+++ src/routes/Cart/style.js
@@ -1,1 +1,16 @@
 import styled from 'styled-components'
+
+export const CartsTitleContainer = styled.div`
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+  margin-left: 2rem;
+`
+
+export const CartsTitle = styled.h2`
+  font-size: 2rem;
+`
+
+export const TitleSpan = styled.span`
+  font-size: 2rem;
+  color: ${(props) => props.theme.palette.baseColor};
+`
