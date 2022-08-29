920a135f1b677340a902b341f37dfe408451f10a
k-m-jin <mel917@naver.com>
2022-08-29 19:17:56 +0900

    #61 design: Update styled components

--- src/components/common/Card/style.js
+++ src/components/common/Card/style.js
@@ -6,18 +6,19 @@ export const CardContainer = styled.div`
   height: 11.9rem;
   display: flex;
   align-items: center;
-  padding: 1.6rem;
+  padding: 1rem;
   position: relative;
   font-size: small;
+  margin: 1.5rem;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
 `
-
-export const CardCheckBox = styled.div``
-
 export const CardBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 6.8rem;
+  flex-shrink: 0;
 `
 
 export const CardImage = styled.img`
@@ -25,34 +26,31 @@ export const CardImage = styled.img`
   height: 100%;
   transform: rotate(90deg);
 `
-
-export const CardName = styled.div`
-  background-color: transparent;
-  width: 14.2rem;
-  margin-right: 1rem;
+export const CardInfo = styled.div`
+  display: flex;
+  flex-flow: column;
+  gap: 1rem;
+  flex-grow: 1;
 `
 export const CardTitle = styled.div`
   display: flex;
   gap: 0.5rem;
 `
-
+export const CardName = styled.div`
+  background-color: transparent;
+  margin-right: 1rem;
+`
 export const CardBank = styled.div`
   background-color: transparent;
 `
 export const CardDescription = styled.div`
   background-color: transparent;
-
   color: #979cac;
-`
-export const CardInfo = styled.div`
-  display: flex;
-  flex-flow: column;
-  gap: 1rem;
 `
 
 export const CardFavorite = styled.div`
-  position: absolute;
   font-size: 2.5rem;
-  top: 2.5rem;
-  right: 2.5rem;
+  flex-shrink: 0;
+  width: 6.8rem;
+  text-align: center;
 `
