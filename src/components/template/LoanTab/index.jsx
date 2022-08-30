bc5ae2f061717504743b73e6f9f3796b2b1979fc
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-30 16:56:11 +0900

    #77 fix: Bank, Loan 분리

--- src/components/template/LoanTab/index.jsx
+++ src/components/template/LoanTab/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import * as S from './style'
+import { useGetProductsQuery } from '../../../store/slices/productApiSlice'
+import Loan from '../../common/Loan'
 
 function LoanTab() {
-  return <div>LoanTab</div>
+  const { data: products, isLoading, isError } = useGetProductsQuery()
+
+  if (isLoading) {
+    return <div>로딩중...</div>
+  }
+
+  if (isError || !products) {
+    return <div>오류발생!</div>
+  }
+  return (
+    <>
+      {products.loan.map((item) => (
+        <Loan item={item} key={item.loan_id} />
+      ))}
+    </>
+  )
 }
 
 export default LoanTab
