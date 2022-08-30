1c58eddd1ab38c39f51eed7ba7128486da0dd835
YANGDAYOUNG <jc1525@hanmail.net>
2022-08-30 15:56:30 +0900

    #71 feat: login, logout기능 -> 각 컴포넌트에 적용필요함.

--- src/store/apis/index.js
+++ src/store/apis/index.js
@@ -3,18 +3,23 @@ import { productsSlice } from '../slices/productApiSlice'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { cartApi } from '../slices/cartApiSlice'
 import { favoritesApi } from '../slices/favoriteApiSlice'
+import { authApi } from '../slices/authApiSlice'
+import authReducer from '../slices/authSlice'
 
 export const store = configureStore({
   reducer: {
     [productsSlice.reducerPath]: productsSlice.reducer,
     [cartApi.reducerPath]: cartApi.reducer,
     [favoritesApi.reducerPath]: favoritesApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       productsSlice.middleware,
       cartApi.middleware,
       favoritesApi.middleware,
+      authApi.middleware,
     ),
 })
 
--- src/store/slices/authApiSlice.js
+++ src/store/slices/authApiSlice.js
@@ -1,1 +1,33 @@
 //로그인, 로그아웃,  토큰 무효화 체크
+const { VITE_BASE_URL } = import.meta.env
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: VITE_BASE_URL,
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`)
+    }
+
+    return headers
+  },
+})
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery,
+  endpoints: (builder) => ({
+    //로그인하기
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: 'login',
+        method: 'POST',
+        body: { ...credentials },
+      }),
+    }),
+  }),
+})
+
+export const { useLoginMutation } = authApi
