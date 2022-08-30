ca4c01a35c02bcb715214b4684fd34b983730224
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-30 16:49:40 +0900

    #77 config: BankTab, LoanTab 생성

---
+++ src/components/template/BankTab/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function BankTab() {
+  return <div>BankTab</div>
+}
+
+export default BankTab
---
+++ src/components/template/LoanTab/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as S from './style'
+
+function LoanTab() {
+  return <div>LoanTab</div>
+}
+
+export default LoanTab
