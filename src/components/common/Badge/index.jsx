cf65ec61bf5bce2d69daed4828a28bf5861f8071
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-01 15:46:28 +0900

    #108 fix: badge 데이터 없는 경우 생기는 에러 수정

--- src/components/common/Badge/index.jsx
+++ src/components/common/Badge/index.jsx
@@ -4,12 +4,8 @@ import * as S from './style'
 
 function Badge() {
   const { data: carts } = useGetCartQuery()
-  console.log(carts.cardList.length + carts.loanList.length)
-  // let num = carts.cardList.length + carts.loanList.length
 
-  // console.log(carts.cardList.length + carts.loanList.length)
-
-  return <S.Badge>{carts.cardList.length + carts.loanList.length}</S.Badge>
+  return <S.Badge>{carts?.cardList.length + carts?.loanList.length}</S.Badge>
 }
 
 export default Badge
