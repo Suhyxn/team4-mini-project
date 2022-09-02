83f38ce1f61bc71b0db1906a3e7579cbaccdb7f5
k-m-jin <mel917@naver.com>
2022-09-02 13:18:14 +0900

    #41 feat: Craete SignUp page

--- src/routes/SignUp/index.jsx
+++ src/routes/SignUp/index.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { createContext, useRef, useState } from 'react'
 import Additional from '../../components/template/Additional'
+import CustomInput from '../../components/common/CustomInput'
+import * as S from './style'
+import SignUpForm from '../../components/signUP/SignUpForm'
+import SignUpModal from '../../components/signUP/SignUpModal'
+
+const initialFormData = {
+  id: '',
+  pw: '',
+  confirmPw: '',
+}
+
+export const FormContext = createContext({
+  formData: initialFormData,
+  setFormData: () => {},
+})
 
 function SignUp() {
+  const [formData, setFormData] = useState(initialFormData)
+  const modalRef = useRef(null)
   return (
     <div>
-      <Additional />
+      <FormContext.Provider value={{ formData, setFormData }}>
+        <S.SignUpContainer>
+          <S.Title>
+            회원가입을 위한 <br /> 정보가 필요해요
+          </S.Title>
+          <SignUpForm modalRef={modalRef}></SignUpForm>
+          <SignUpModal ref={modalRef} />
+        </S.SignUpContainer>
+      </FormContext.Provider>
+      {/* <Additional /> */}
     </div>
   )
 }
--- src/routes/SignUp/style.js
+++ src/routes/SignUp/style.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components'
+
+export const SignUpContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const Title = styled.h2`
+  margin-bottom: 6rem;
+  font-size: 2.2rem;
+  line-height: 3.4rem;
+`
