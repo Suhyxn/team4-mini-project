cad17a4dcbdaafbfab5c55d0e02b3788bfc07af7
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-06 02:53:14 +0900

    fix: 로그인 api연결

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -19,7 +19,7 @@ function Login() {
   const [login, { isLoading }] = useLoginMutation()
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  // const [errMsg, setErrMsg] = useState('')
+  const [errMsg, setErrMsg] = useState('')
   const [userData, setUserData] = useState(null)
 
   // let [isActive, setIsActive] = useState(false)
@@ -32,22 +32,17 @@ function Login() {
       dispatch(setCredentials(userData['data']))
       navigate('../recommened')
     } catch (error) {
-      // if (!error?.originalStatus) {
-      //   // isLoading: true until timeout occurs
-      //   // setErrMsg('No Server Response')
-      //   return console.log('No Server Response')
-      // } else if (error.originalStatus === 400) {
-      //   // setErrMsg('Missing Username or Password')
-      //   return console.log('Missing Username or Password')
-      // } else if (error.originalStatus === 401) {
-      //   return console.log('Unauthorized')
-
-      //   // setErrMsg('Unauthorized')
-      // } else {
-      //   // setErrMsg('Login Failed')
-      //   return console.log('Login Failed')
-      // }
-      console.log('error')
+      if (!error?.originalStatus) {
+        // isLoading: true until timeout occurs
+        setErrMsg('No Server Response')
+        return console.log('No Server Response')
+      } else if (error.originalStatus === 400) {
+        setErrMsg('Missing Username or Password')
+      } else if (error.originalStatus === 401) {
+        setErrMsg('Unauthorized')
+      } else {
+        setErrMsg('Login Failed')
+      }
     }
   }
   // const onClickHandler = () => {
@@ -60,33 +55,33 @@ function Login() {
         <div>Loading...</div>
       ) : (
         <S.Container>
-          {/* {errMsg ? (
+          {errMsg ? (
             <S.Title>{errMsg}</S.Title>
-          ) : ( */}
-          <>
-            <S.Title>로그인 한 번이면 간편하게!</S.Title>
-            <CustomInput
-              disabled="disabled"
-              active={active}
-              userInput={userInput}
-              inputBlurHandler={inputBlurHandler}
-              inputHandler={inputHandler}
-              onUserInputChange={onUserInputChange}
-            />
-            <S.BtnBox>
-              <Button size="medium" onClick={onClickHandler}>
-                로그인
-              </Button>
-              <Button
-                size="medium"
-                disabled={active}
-                onClick={() => navigate('/signup')}
-              >
-                회원가입
-              </Button>
-            </S.BtnBox>
-          </>
-          {/* )} */}
+          ) : (
+            <>
+              <S.Title>로그인 한 번이면 간편하게!</S.Title>
+              <CustomInput
+                disabled="disabled"
+                active={active}
+                userInput={userInput}
+                inputBlurHandler={inputBlurHandler}
+                inputHandler={inputHandler}
+                onUserInputChange={onUserInputChange}
+              />
+              <S.BtnBox>
+                <Button size="medium" onClick={onClickHandler}>
+                  로그인
+                </Button>
+                <Button
+                  size="medium"
+                  disabled={active}
+                  onClick={() => navigate('/signup')}
+                >
+                  회원가입
+                </Button>
+              </S.BtnBox>
+            </>
+          )}
         </S.Container>
       )}
     </div>
