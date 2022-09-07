dd5b055716727fdfcb1bb98e867a20290ce325c3
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-09-07 23:23:06 +0900

    fix: 문구 수정

--- src/components/template/LoanInfo/index.jsx
+++ src/components/template/LoanInfo/index.jsx
@@ -10,15 +10,15 @@ function LoanInfo() {
       <S.Title>
         나에게 딱 맞는
         <br />
-        신용대출을 찾아보세요
+        신용상품을 찾아보세요
       </S.Title>
       <S.SubTitle>
-        다양한 금융사의 신용대출 금리와 한도를
+        다양한 금융사의 신용 금리와 한도를
         <br />
         비교할 수 있어요
       </S.SubTitle>
       <Button size="large" width="80%" onClick={() => navigate('/login')}>
-        내 조건으로 대출 찾기
+        내 조건으로 상품 찾기
       </Button>
     </S.Container>
   )
