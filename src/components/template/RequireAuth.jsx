282307c1c61e4ade6eb8d0f8c812e6dedd4a373e
YANGDAYOUNG <jc1525@hanmail.net>
2022-09-07 17:16:04 +0900

    fix: 쿠키 바로 가져와서 로그인 페이지에서 탈출!!!!!! 야호!!!!!!!

--- src/components/template/RequireAuth.jsx
+++ src/components/template/RequireAuth.jsx
@@ -1,12 +1,14 @@
 import { useLocation, Navigate, Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { selectCurrentToken } from '../../store/slices/authSlice'
-
+import { Cookies } from 'react-cookie'
 const RequireAuth = () => {
   const location = useLocation()
+  const cookies = new Cookies()
+  const token = cookies.get('accessToken')
   return (
     <>
-      {selectCurrentToken ? (
+      {token ? (
         <Outlet />
       ) : (
         <Navigate to="/login" state={{ from: location }} replace />
--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -7,7 +7,7 @@ import {
   useLoginMutation,
   useGetIsduplicateQuery,
 } from '~/store/slices/authApiSlice'
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router-dom'
 import { Cookies } from 'react-cookie'
 import { setCredentials } from '../../store/slices/authSlice'
 import useInputValue from '../../components/Hook/useInputValue'
@@ -28,7 +28,8 @@ function Login() {
     try {
       const userData = await login({ username: id, password: pwd })
       dispatch(setCredentials(userData['data']))
-      navigate('../favorites')
+      navigate('/favorites', { replace: true })
+      console.log('successxxx')
     } catch (error) {
       if (!error?.originalStatus) {
         setErrMsg('No Server Response')
